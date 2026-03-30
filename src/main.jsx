ae43cacbbb4c84f6f4f8eb3f2b9202379c97b415
Swayam-Jaswal <swayamjaswalyt@gmail.com>
2026-03-30 23:58:32 +0530

    changes main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Providers } from './app/providers.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/app/App';
+import '@/main.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Providers />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
